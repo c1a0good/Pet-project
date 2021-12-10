59663f803ea22689d0f7f0d4903d50e9e6b227d3
Andrey Loginov <loginov@itspartner.net>
2021-12-10 18:04:22 +0300

    [S2T1] Changed tests in response to PR comments

--- src/test/java/com/project/pet/repository/JdbcUserRepositoryTest.java
+++ src/test/java/com/project/pet/repository/JdbcUserRepositoryTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.*;
@@ -25,11 +27,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DataJdbcTest
 @Sql({"classpath:sqlScripts/users.sql", "classpath:testData/users_data.sql"})
 public class JdbcUserRepositoryTest {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     private final JdbcUserRepository userRepository;
 
     @Autowired
     public JdbcUserRepositoryTest(NamedParameterJdbcTemplate jdbcTemplate) {
+
+        this.jdbcTemplate = jdbcTemplate;
 
         this.userRepository = new JdbcUserRepository(jdbcTemplate);
     }
@@ -51,8 +56,9 @@ public class JdbcUserRepositoryTest {
     @Test
     public void shouldReadUserById() {
 
-        User expectedUser = new User(1, "name1", "surname1", 18, "+375291234567"),
-                actualUser = userRepository.readById(1L);
+        User expectedUser = new User(1, "name1", "surname1", 18, "+375291234567");
+
+        User actualUser = userRepository.readById(1L);
 
         assertEquals(expectedUser, actualUser);
     }
@@ -64,7 +70,7 @@ public class JdbcUserRepositoryTest {
 
         userRepository.create(expectedUser);
 
-        User actualUser = userRepository.readById(4L);
+        User actualUser = readById(4L);
 
         assertEquals(expectedUser, actualUser);
     }
@@ -76,7 +82,7 @@ public class JdbcUserRepositoryTest {
 
         userRepository.update(expectedUser);
 
-        User actualUser = userRepository.readById(1L);
+        User actualUser = readById(1L);
 
         assertEquals(expectedUser, actualUser);
     }
@@ -91,8 +97,45 @@ public class JdbcUserRepositoryTest {
 
         userRepository.delete(1L);
 
-        Collection<User> actualUsers = userRepository.readAll();
+        Collection<User> actualUsers = readAll();
 
         assertEquals(expectedUsers, actualUsers);
     }
+
+    private User readById(Long id) {
+
+        String sql = "SELECT * FROM users " +
+                "WHERE user_id = :user_id";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+
+        params.addValue("user_id", id);
+
+        return getUsersThroughSqlQuery(sql, params).get(0);
+    }
+
+    private Collection<User> readAll() {
+
+        String sql = "SELECT * FROM users";
+
+        return getUsersThroughSqlQuery(sql, new MapSqlParameterSource());
+    }
+
+    private List<User> getUsersThroughSqlQuery(String sql, MapSqlParameterSource params) {
+
+        return jdbcTemplate.query(sql, params,
+                (resultSet, i) -> {
+                    User user = new User();
+
+                    user.setId(resultSet.getLong("user_id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setSurname(resultSet.getString("surname"));
+                    user.setAge(resultSet.getInt("age"));
+                    user.setPhoneNumber(resultSet.getString("phone_number"));
+
+                    return user;
+                });
+    }
+
+
 }
